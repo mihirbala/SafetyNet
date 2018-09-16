abaf78201dde98f8360dd0c34b9e486f18f7b1a1
Sam Lee <leesamu@umich.edu>
2018-09-15 21:20:27 -0400

    Tons of flask stuff

--- app/safetynet_server.py
+++ app/safetynet_server.py
@@ -1,15 +1,35 @@
+from __future__ import print_function
+from flask import Flask, render_template, request, Response, url_for, jsonify
+
 import socketserver
 import os
+import time
 import sys
 
-from flask import Flask, render_template, request, url_for, jsonify
 
 app = Flask(__name__)
 default_port = 8500
 
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.enable_buffering(5)
+    return rv
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("server_home.html")
+    if request.method == "POST":
+        lat = request.form["latitude"]
+        lon = request.form["longitude"]
+        #print "hello"
+        #print("hello", file=sys.stderr)
+        print(lat, file=sys.stderr)
+        print(lon, file=sys.stderr)
+
+        return jsonify({"latitude": lat, "longitude": lon})
+
+    return render_template("server_home.html", latitude="na", longitude="na")
 
 # For starting the server
 def main():
@@ -22,6 +42,8 @@ def main():
         if default_port >= 9000:
             default_port = 8001
         try:
+            with open("./data/port.txt", "w") as f:
+                f.write(str(default_port))
             app.run(debug=True, host="0.0.0.0", port=default_port)
         except socketserver.socket.error as sse:
             # Port didn't work, trying another
@@ -34,8 +56,6 @@ def main():
             return
         break
 
-    with open("./data/port.txt", "w") as f:
-        f.write(str(default_port))
 
 
 if __name__ == "__main__":
